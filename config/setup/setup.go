b5e203f26ca6bee4d0ed509aca864f05582a0204
venus <venus@example.com>
2024-08-25 15:25:56 -0300

    feat: add functionality to create config directory and file
    the code will use the envars
    if they dont exists will use defaults
      in config file case will download the example config

---
+++ config/setup/setup.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"os"
+	"path/filepath"
+)
+
+func createConfig() {
+	var source_file io.Reader
+	example_config_path := os.Getenv("TWC_EXAMPLE_CONFIG_PATH")
+
+	if example_config_path == "" {
+		url := "https://raw.githubusercontent.com/rodrigo-sys/telescope-mantoc/main/.env.example"
+		response, _ := http.Get(url)
+		defer response.Body.Close()
+		source_file = response.Body
+		// defer source_file.Close()
+		// td: replace <user> wih os.UserConfigDir
+	} else {
+		source_file, _ = os.Open(example_config_path)
+		// defer source_file.Close()
+	}
+
+	destination_file, _ := os.Create(config_path)
+	defer destination_file.Close()
+	io.Copy(destination_file, source_file)
+}
+
+func SetupConfig() {
+	/*td: think to do this in a setup or installer script just one time*/
+	
+	// create config folder
+	config_path := getConfigPath()
+	os.MkdirAll(filepath.Dir(config_path), os.ModePerm)
+
+	// create config file
+	if _, err := os.Stat(config_path); os.IsNotExist(err) {
+		createConfig()
+	}
+}
