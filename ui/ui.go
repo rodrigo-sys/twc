5b511accd93dfd3d1abe338a41bb2677fd545144
rodirgo <rodrigo@chavez.com>
2024-10-26 13:36:02 -0300

    wip (ui): implement item interface with custom item

---
+++ ui/ui.go
@@ -0,0 +1,37 @@
+package ui
+
+import (
+	. "twc/types"
+
+	"github.com/charmbracelet/bubbles/list"
+)
+
+/* ITEM */
+type itemWrapper[T any] struct {
+	data    T
+	sublist list.Model
+}
+
+func (i itemWrapper[T]) Title() string {
+	switch i := any(i.data).(type) {
+	case Channel:
+		return i.Name
+	case Video:
+		return i.Name
+	}
+	return ""
+}
+
+func (i itemWrapper[T]) Description() string {
+	return ""
+}
+
+func (i itemWrapper[T]) FilterValue() string {
+	switch i := any(i.data).(type) {
+	case Channel:
+		return i.Name
+	case Video:
+		return i.Name
+	}
+	return ""
+}
