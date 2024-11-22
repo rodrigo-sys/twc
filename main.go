cf3a08ee4b06efae5ed25a404fc4b9fee9d86f3f
rodirgo <rodrigo@chavez.com>
2024-11-22 18:54:03 -0300

    chore: clean code

--- main.go
+++ main.go
@@ -3,9 +3,8 @@ package main
 import (
 	. "twc/channels"
 	config "twc/config/utils"
-	"twc/ui"
-
 	. "twc/types"
+	"twc/ui"
 	// . "twc/videos"
 )
 
@@ -13,11 +12,10 @@ func main() {
 	config.SetupConfig()
 
 	var channels Channels
-	// /*
 	channels.GetChannels()
 	channels.CheckStatus()
-	// channels.CheckStatusSync()
 	channels.SortChannels()
+
 	choice := ui.Menu(channels)
 
 	switch choice := choice.(type) {
@@ -26,23 +24,22 @@ func main() {
 	case ui.ItemWrapper[Video]:
 		choice.Data.Platform.OpenVod(choice.Data)
 	}
-	// */
 
-	/*
-		channels.GetChannels()
-		channels.CheckStatus()
-		channels.SortChannels()
+	/* legacy menu
+	channels.GetChannels()
+	channels.CheckStatus()
+	channels.SortChannels()
 
-		selected := channels.Menu()
+	selected := channels.Menu()
 
-		if selected.Islive {
-			selected.Platform.OpenChannel(selected)
-		} else {
-			//td: return Videos type in GetVods
-			var videos Videos
-			videos = selected.Platform.GetVods(selected)
-			vod := videos.Menu()
-			vod.Platform.OpenVod(vod)
-		}
+	if selected.Islive {
+		selected.Platform.OpenChannel(selected)
+	} else {
+		//td: return Videos type in GetVods
+		var videos Videos
+		videos = selected.Platform.GetVods(selected)
+		vod := videos.Menu()
+		vod.Platform.OpenVod(vod)
+	}
 	*/
 }
