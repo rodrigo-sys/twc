4c5c608c777337174d11c3871e2e424441276123
rodirgo <rodrigo@chavez.com>
2024-11-30 12:25:20 -0300

    create the Video interface and a BaseVideo struct that embeds it

---
+++ video/video.go
@@ -0,0 +1,69 @@
+package video
+
+import (
+	"fmt"
+	"log"
+	"os/exec"
+
+	"github.com/koki-develop/go-fzf"
+)
+
+type Videos []Video
+
+type Video interface {
+	Open()
+	Name() string
+	Url() string
+
+	SetName(name string)
+	SetUrl(url string)
+}
+
+// td: maybe change Name for Title
+type BaseVideo struct {
+	name string
+	url  string
+}
+
+/* methods */
+func (v *BaseVideo) Open() {
+	output, err := exec.Command("mpv", v.url).CombinedOutput()
+	if err != nil {
+		fmt.Println(string(output))
+	}
+}
+
+/* getters */
+func (v *BaseVideo) Name() string {
+	return v.name
+}
+func (v *BaseVideo) Url() string {
+	return v.url
+}
+
+/* setters */
+func (v *BaseVideo) SetName(name string) {
+	v.name = name
+}
+func (v *BaseVideo) SetUrl(url string) {
+	v.url = url
+}
+
+/* this is the legacy menu */
+func (videos Videos) Menu() Video {
+	f, err := fzf.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	idxs, err := f.Find(videos, func(i int) string { return videos[i].Name() })
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, i := range idxs {
+		return videos[i]
+	}
+
+	return &BaseVideo{}
+}
