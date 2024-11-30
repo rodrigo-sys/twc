196dea40d2787241aa00daf8ce46830efb36e91c
rodirgo <rodrigo@chavez.com>
2024-11-30 12:10:26 -0300

    create the Channel interface and a BaseChannel struct that embeds it

---
+++ channel/channel.go
@@ -0,0 +1,58 @@
+package channel
+
+import . "twc/video"
+
+type Channel interface {
+	CheckStatus() bool
+	GetUrl() string
+	GetVods() Videos
+	OpenChannel()
+
+	Name() string
+	Islive() bool
+	Position() int
+
+	SetName(name string)
+	SetIslive(islive bool)
+	SetPosition(position int)
+}
+
+type BaseChannel struct {
+	name     string
+	position int
+	islive   bool
+}
+
+func (c *BaseChannel) GetUrl() string {
+	return ""
+}
+func (c *BaseChannel) CheckStatus() bool {
+	return false
+}
+func (c *BaseChannel) OpenChannel() {
+}
+func (c *BaseChannel) GetVods() Videos {
+	return Videos{}
+}
+
+/* getters */
+func (c *BaseChannel) Name() string {
+	return c.name
+}
+func (c *BaseChannel) Position() int {
+	return c.position
+}
+func (c *BaseChannel) Islive() bool {
+	return c.islive
+}
+
+/* setters */
+func (c *BaseChannel) SetName(name string) {
+	c.name = name
+}
+func (c *BaseChannel) SetIslive(islive bool) {
+	c.islive = islive
+}
+func (c *BaseChannel) SetPosition(position int) {
+	c.position = position
+}
