be93fa4596f97a91bc6701e2bb3974749da4c5de
rodirgo <rodrigo@chavez.com>
2024-12-21 12:39:22 -0300

    feat(ospackage): add install method with regular package managers support

--- installer/ospackage.go
+++ installer/ospackage.go
@@ -2,4 +2,20 @@ package installer
 type Ospackage struct {
 	Name            string
 	Install_method  string
+func (p Ospackage) Install() error {
+	if IsInstalled(p.Name) {
+		return nil
+	}
+
+	switch p.Install_method {
+	case "winget":
+		return exec.Command("winget", "install", "--id", p.Name, "-e", "--accept-package-agreements", "--accept-source-agreements").Run()
+	case "apt":
+		return exec.Command("sh", "-c", "sudo apt-get install "+p.Name+" -y").Run()
+	case "pacman":
+		return exec.Command("sh", "-c", "sudo pacman -S "+p.Name+" --noconfirm").Run()
+	}
+
+	return nil
+}
 }
