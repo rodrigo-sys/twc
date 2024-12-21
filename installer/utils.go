0c28353bd854be5359abc2c3360c2e410d3b1b17
rodirgo <rodrigo@chavez.com>
2024-12-21 13:47:45 -0300

    feat(installer): add wrapper function to install ospackages

--- installer/utils.go
+++ installer/utils.go
@@ -14,3 +14,21 @@ func IsInstalled(program string) bool {
 	return err == nil
 	// return os.IsExist(err)
 }
+func DefaultInstallStrategy(ospackage_names ...string) error {
+	var ospackages Ospackages
+	if len(ospackage_names) == 3 {
+		ospackages = Ospackages{
+			"has_apt":    Ospackage{Name: ospackage_names[0], Install_method: "apt"},
+			"has_pacman": Ospackage{Name: ospackage_names[1], Install_method: "pacman"},
+			"has_winget": Ospackage{Name: ospackage_names[2], Install_method: "winget"},
+		}
+	} else {
+		ospackages = Ospackages{
+			"has_apt":    Ospackage{Name: ospackage_names[0], Install_method: "apt"},
+			"has_pacman": Ospackage{Name: ospackage_names[0], Install_method: "pacman"},
+			"has_winget": Ospackage{Name: ospackage_names[0], Install_method: "winget"},
+		}
+	}
+
+	return ospackages.Install()
+}
