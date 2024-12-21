bb3595cd1e4b13f83cd7d42d456bf943b7efcf5e
rodirgo <rodrigo@chavez.com>
2024-12-21 13:26:07 -0300

    feat(ospackages): add method to select ospackage for current system

--- installer/ospackage.go
+++ installer/ospackage.go
@@ -20,4 +20,44 @@ func (p Ospackage) Install() error {
 }
 type system_type string
 type Ospackages map[system_type]Ospackage
+func (system_ospackages Ospackages) Select() (Ospackage, error) {
+	if pkg, ok := system_ospackages["any"]; ok {
+		return pkg, nil
+	}
+
+	switch runtime.GOOS {
+	case "linux":
+		if pkg, ok := system_ospackages["linux"]; ok {
+			return pkg, nil
+		}
+
+		if IsInstalled("apt") {
+			if pkg, ok := system_ospackages["has_apt"]; ok {
+				return pkg, nil
+			}
+		} else if IsInstalled("pacman") {
+			if pkg, ok := system_ospackages["has_aurhelper"]; ok {
+				if IsInstalled(pkg.Aurhelper) {
+					return pkg, nil
+				}
+			}
+
+			if pkg, ok := system_ospackages["has_pacman"]; ok {
+				return pkg, nil
+			}
+
+		} else {
+			return Ospackage{}, ErrUnsupportedPkgMgr
+		}
+	case "windows":
+		if IsInstalled("winget") {
+			if pkg, ok := system_ospackages["has_winget"]; ok {
+				return pkg, nil
+			}
+		} else {
+			return Ospackage{}, ErrWingetNotInstalled
+		}
+	}
+
+	return Ospackage{}, nil
 }
