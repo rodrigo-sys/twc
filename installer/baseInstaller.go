8d390247a4e278704160e71e540a130f84a00bc8
rodirgo <rodrigo@chavez.com>
2024-12-21 18:57:27 -0300

    feat(baseInstaller): add installchat method

--- installer/baseInstaller.go
+++ installer/baseInstaller.go
@@ -1,7 +1,33 @@
 package installer
+import (
+	"fmt"
+)
+var (
+	ErrorOfficialChatEmpty   = fmt.Errorf("official_chat cannot be empty")
+)
+
 type BaseInstaller struct {
 	chat            string
 	player          string
 	official_chat   string
 	official_player string
+}
+func (b BaseInstaller) InstallChat() error {
+	if b.official_chat == "" {
+		return ErrorOfficialChatEmpty
+	}
+
+	fmt.Println("installing chat")
+
+	// if chat is popout just exit
+	if b.chat == "popout" {
+		return nil
+	}
+
+	if b.chat != "" && b.chat != b.official_chat {
+		fmt.Println("installing " + b.chat)
+		return DefaultInstallStrategy(b.chat)
+	}
+
+	return b.InstallOfficialChat()
 }
