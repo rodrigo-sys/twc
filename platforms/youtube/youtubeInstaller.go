af6a252f2466fbd6a8b8bced81aa69e41870b9d9
rodirgo <rodrigo@chavez.com>
2024-12-22 11:00:44 -0300

    feat(youtubeInstaller): add installOfficialChat method

--- platforms/youtube/youtubeInstaller.go
+++ platforms/youtube/youtubeInstaller.go
@@ -1,6 +1,7 @@
 package platforms
 
 import (
+	"fmt"
 	. "twc/installer"
 )
 
@@ -8,3 +9,22 @@ type YoutubeInstaller struct {
 	BaseInstaller
 }
 
+func (y YoutubeInstaller) InstallOfficialChat() error {
+	// exit if pytchatty is already installed
+	if IsInstalled("pytchatty") {
+		return nil
+	}
+
+	// install git
+	fmt.Println("installing git")
+	err := InstallGit()
+	if err != nil {
+		return fmt.Errorf("Error intalling git: %w", err)
+	}
+
+	// install pytchatty
+	fmt.Println("installing pytchatty")
+	Ospackage{Name: "git+https://github.com/rodrigo-sys/pytchatty", Install_method: "pipx"}.Install()
+
+	return nil
+}
