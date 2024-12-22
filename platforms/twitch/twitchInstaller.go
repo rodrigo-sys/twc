c474fa9115e14892b554943f5b5674f58c1ebc78
rodirgo <rodrigo@chavez.com>
2024-12-22 10:44:36 -0300

    feat(twitchInstaller): add installOfficialChat method

--- platforms/twitch/twitchInstaller.go
+++ platforms/twitch/twitchInstaller.go
@@ -1,7 +1,26 @@
 package platforms
+
 import (
+	"fmt"
 	. "twc/installer"
 )
+
 type TwitchInstaller struct {
 	BaseInstaller
+}
+
+func (y TwitchInstaller) InstallOfficialChat() error {
+	// exit if chatterino is already installed
+	if IsInstalled("chatterino") || IsInstalled("com.chatterino.chatterino") || IsInstalled("/var/lib/flatpak/exports/bin/com.chatterino.chatterino") {
+		return nil
+	}
+
+	// install chatterino
+	fmt.Println("installing chatterino")
+	return Ospackages{
+		"has_apt":       Ospackage{Name: "app/com.chatterino.chatterino/x86_64/stable", Install_method: "flatpak"},
+		"has_aurhelper": Ospackage{Name: "chatterino2-git ", Install_method: "aurhelper"},
+		"has_pacman":    Ospackage{Name: "app/com.chatterino.chatterino/x86_64/stable", Install_method: "flatpak"},
+		"has_winget":    Ospackage{Name: "ChatterinoTeam.Chatterino", Install_method: "winget"},
+	}.Install()
 }
