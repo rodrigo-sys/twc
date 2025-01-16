33f4fe9914893fc6613d12aeead2e7ab4e420da3
rodirgo <rodrigo@chavez.com>
2025-01-16 14:36:19 -0300

    feat(youtube): add GetPopoutChatUrl method

--- platforms/youtube/youtube.go
+++ platforms/youtube/youtube.go
@@ -22,6 +22,10 @@ func (y Youtube) GetUrl() string {
 	return url
 }
 
+func (y Youtube) GetPopoutChatUrl() string {
+	id, _ := exec.Command("yt-dlp", "--print", "id", y.GetUrl()).Output()
+	return fmt.Sprintf("https://www.youtube.com/live_chat?is_popout=1&v=%s", id)
+}
 func (y Youtube) CheckStatus() bool {
 	response, error := http.Get(y.GetUrl())
 	if error != nil {
