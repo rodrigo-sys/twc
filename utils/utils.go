7aec2f0a3a669facd27f9722006f2f0f6fe3657c
rodirgo <rodrigo@chavez.com>
2025-01-22 18:21:03 -0300

    feat(utils): add function to open files with default app
    It calls the corresponding os opener (start, open, xdg-open)
    Supports Windows, macOS, and Linux.
    Handles terminal-based applications on Linux.

--- utils/utils.go
+++ utils/utils.go
@@ -1,7 +1,13 @@
 package utils
 
 import (
+	"fmt"
+	"os"
 	"os/exec"
+	"regexp"
+	"runtime"
+	"strings"
+	"syscall"
 )
 
 func CloudScraperGet(url string) string {
@@ -27,3 +33,47 @@ print(scraper.get("%s").text)`
 	return string(output)
 }
 */
+
+func OpenWithDefaultApp(filePath string) error {
+	var cmd *exec.Cmd
+
+	switch runtime.GOOS {
+	case "windows":
+		cmd = exec.Command("cmd", "/c", "start", filePath)
+	case "darwin":
+		cmd = exec.Command("open", filePath)
+	case "linux":
+		desktop, _ := exec.Command("xdg-mime", "query", "default", "text/plain").Output()
+		desktop_file, _ := os.ReadFile("/usr/share/applications/" + strings.TrimSpace(string(desktop)))
+		if strings.Contains(string(desktop_file), "Terminal=true") {
+			for _, line := range strings.Split(string(desktop_file), "\n") {
+				if strings.HasPrefix(line, "Exec=") {
+					exec_value := strings.TrimSpace(strings.TrimPrefix(line, "Exec="))
+
+					r := regexp.MustCompile("%f|%F")
+					cmd_string := r.ReplaceAllString(exec_value, filePath)
+
+					splited := strings.Split(cmd_string, " ")
+					cmd = exec.Command(os.Getenv("TERMINAL"), splited...)
+					break
+				}
+			}
+		} else {
+			cmd = exec.Command("xdg-open", filePath)
+		}
+
+	default:
+		return fmt.Errorf("unsupported os: %s", runtime.GOOS)
+	}
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setsid: true}
+	return cmd.Start()
+
+	/*
+		fmt.Printf("%#v\n", cmd.Args)
+		output, err := cmd.CombinedOutput()
+		fmt.Println(err.Error())
+		fmt.Println(string(output))
+		return nil
+	*/
+}
